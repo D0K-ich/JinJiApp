529638b80ed88555ccfee56973f06cceca79cc15
klownKrico <vasilisamosse@yandex.ru>
2024-07-26 14:14:58 +0300

    фон для основной страницы

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,43 +1,17 @@
 <template>
   <v-app style="background: none">
-      <navbar v-if="ready"/>
-      <Preloader :percent_loaded="percent_loaded" v-if="!ready"/>
-  </v-app>
 
+    <main-page/>
+
+
+  </v-app>
 </template>
 
 <script lang="ts" setup>
-import Preloader from "./views/preloader/Preloader.vue";
-import {onMounted, ref} from "vue";
-import {CloseWebsocket, CreateWebsocket} from "./network/websocket";
-import Navbar from "./views/navigation/navbar.vue";
 
-const percent_loaded  = ref<number>(0)
-const ready = ref<boolean>(false)
-
-onMounted(() => {
-  CreateWebsocket("ws://localhost:8080/ws")
-
-  setInterval(() => {
-    percent_loaded.value += Math.floor( Math.random() * 10)
-    if (percent_loaded.value >= 100) {ready.value = true; CloseWebsocket()}
-  }, 100)
-})
-
+import MainPage from "./views/pages/MainPage.vue";
 </script>
 
 <style lang="scss">
-  html {
-    overflow: hidden!important;
-    background: linear-gradient(#000000, #310E37)
-  }
-
-  a {
-    color: #013a54;
-    text-decoration: none;
-    &:hover {
-      color: #0579a8;
-    }
-  }
 
 </style>
---
+++ frontend/src/views/pages/MainPage.vue
@@ -0,0 +1,18 @@
+<template >
+  <div class="grddd" style="height: 100%; width: 100%">
+
+  </div>
+
+</template>
+
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+ .grddd {
+   background: linear-gradient(167deg, #000000, 70%, #3A114D);
+ }
+
+</style>
